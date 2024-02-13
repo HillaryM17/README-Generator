3b4f9c4358ce48327a5b75fc36915b8f9e8d103b
Hillary Mason <hillary.mason@outlook.com>
2024-02-13 14:45:53 +0000

    updated license code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,22 @@
-const licenseBadge = (answers) => {
-    let license = "";
-    if (answers.license === "MIT"){
-        license = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
-    }
-    else if (answers.license === "Apache License 2.0"){
-        license = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-    }
-    else if (answers.license === "Mozilla Public License 2.0"){
-        license = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
-    }
-    else if (answers.license === "GNU General Public License v3"){
-        license = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
-    }
-    else if (answers.license === "BSD 3-Clause License"){
-        license = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
-    }
-    return console.log(license);
-}
-
-//["MIT", "Apache License 2.0","Mozilla Public License 2.0", "GNU General Public License v3", "BSD 3-Clause License"]
-
+// const licenseBadge = (answers) => {
+//     let license = "";
+//     if (answers.license === "MIT"){
+//         license = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+//     }
+//     else if (answers.license === "Apache License 2.0"){
+//         license = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+//     }
+//     else if (answers.license === "Mozilla Public License 2.0"){
+//         license = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+//     }
+//     else if (answers.license === "GNU General Public License v3"){
+//         license = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+//     }
+//     else if (answers.license === "BSD 3-Clause License"){
+//         license = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+//     }
+//     return console.log(license);
+// }
 
 // function to generate markdown for README
 const generateMarkdown = (answers, license) => {
